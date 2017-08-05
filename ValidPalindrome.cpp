dcbca87902afee6225edc3da1c7ae22ecccb02dd
wwwwpkpkpk <wwwwpkpkpk@gmail.com>
2017-08-05 15:31:39 -0500

    Create ValidPalindrome.cpp

---
+++ ValidPalindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int begin = 0, end = s.length() - 1;
+        
+        while(begin < end) {
+            if(!isalnum(s[begin])) {
+                begin++;
+            }else if(!isalnum(s[end])) {
+                end--;
+            } else {
+                if(tolower(s[begin++]) != tolower(s[end--])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
